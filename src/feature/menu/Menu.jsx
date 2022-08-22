a5d34a0db60fd2e12104a38b9884a6385d79f33f
faisalsaid <falsdesain@gmail.com>
2022-08-22 15:24:54 +0900

    fetch menu from db and put to menu reducer

--- src/feature/menu/Menu.jsx
+++ src/feature/menu/Menu.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchAllMenu } from './menuSlice';
+import { useState, useEffect } from 'react';
 
 const Menu = () => {
+  const dispatch = useDispatch();
+  const { isLoading, data, error } = useSelector((state) => state.menu.menu);
+  useEffect(() => {
+    dispatch(fetchAllMenu());
+  }, []);
+
   return <div>Menu</div>;
 };
 
--- src/feature/menu/menuSlice.js
+++ src/feature/menu/menuSlice.js
@@ -1,4 +1,11 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+// fetch data all menu from API
+
+export const fetchAllMenu = createAsyncThunk('menu/fetchAllMenu', () => {
+  return axios.get('http://localhost:3004/menu').then((resp) => resp.data);
+});
 
 const initialState = {
   menu: { isloading: false, data: [], error: '' },
@@ -7,10 +14,22 @@ const initialState = {
 export const menuSlice = createSlice({
   name: 'menu',
   initialState,
-  reducers: {
-    fetchMenuAPI: (state, action) => {},
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchAllMenu.pending, (state, { payload }) => {
+      state.menu.isloading = true;
+    });
+    builder.addCase(fetchAllMenu.fulfilled, (state, { payload }) => {
+      state.menu.isloading = false;
+      state.menu.data = payload;
+      state.menu.error = '';
+    });
+    builder.addCase(fetchAllMenu.rejected, (state, action) => {
+      state.menu.isloading = false;
+      state.menu.data = [];
+      state.menu.error = action.error.message;
+    });
   },
 });
 
-export const { fetchMenuAPI } = menuSlice.actions;
 export default menuSlice.reducer;

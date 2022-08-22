5709b482f97481d7e63bed18ef1be0a7a325b6ae
faisalsaid <falsdesain@gmail.com>
2022-08-22 18:05:15 +0900

    create function filter unique array of object, add iconCategory to db

--- src/feature/menu/Menu.jsx
+++ src/feature/menu/Menu.jsx
@@ -11,6 +11,15 @@ import MenuCard from './MenuCard';
 const Menu = () => {
   const dispatch = useDispatch();
   const { isLoading, data, error } = useSelector((state) => state.menu.menu);
+  const onlyUnique = (value, index, self) => {
+    return self.indexOf(value) === index;
+  };
+
+  // !important [DON'T DELETE] = this function to get uniq singgel array from arry of object
+  // const menuCategories = [...new Set(data.map((item) => item.category))];
+
+  // !important [DON'T DELETE] this function to get unic object from array of object depends the key of object
+  const newData = [...new Map(data.map((item) => [item['category'], item])).values()];
 
   useEffect(() => {
     dispatch(fetchAllMenu());
@@ -25,8 +34,16 @@ const Menu = () => {
           <p>No Data : {error}</p>
         ) : (
           <Stack direction={'row'} spacing={2}>
-            <div>category 1</div>
-            <div>category 3</div>
+            {newData.map((menu) => (
+              <Card key={menu.id}>
+                <CardMedia component="img" image={menu.iconCategory} height="60" alt="vietnam drip" />
+                <CardContent>
+                  <Typography variant="body1" color="secondary">
+                    {menu.category}
+                  </Typography>
+                </CardContent>
+              </Card>
+            ))}
           </Stack>
         )}
 

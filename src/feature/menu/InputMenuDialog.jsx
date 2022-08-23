af0164dcf74603ea22a6ab2bab50ba0015663f73
faisalsaid <falsdesain@gmail.com>
2022-08-23 15:31:38 +0900

    create add new menu ui

--- src/feature/menu/InputMenuDialog.jsx
+++ src/feature/menu/InputMenuDialog.jsx
@@ -1,15 +1,140 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Dialog, DialogTitle } from '@mui/material';
+import {
+  Box,
+  Button,
+  Dialog,
+  DialogTitle,
+  Divider,
+  IconButton,
+  Typography,
+  Stack,
+  TextField,
+  FormControl,
+  FormHelperText,
+  InputLabel,
+  InputAdornment,
+  OutlinedInput,
+  Autocomplete,
+  Grid,
+} from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
 
 const InputMenuDialog = (props) => {
-  const { open, onClose } = props;
+  const {
+    open,
+    onClose,
+    data: { title, icon },
+  } = props;
 
+  const [formValue, setFormValue] = useState({
+    id: 100,
+    title: '',
+    price: 0,
+    isAvailable: true,
+    category: '',
+    image: '',
+  });
+
+  const [menuName, setMenuName] = useState('');
+  const [price, setPrice] = useState(0);
+  const [categoryValue, setCategoryValue] = useState('');
+  const [categoryInputValue, setCategoryInputValue] = useState('');
+
+  const handleSubmit = (e) => {
+    setFormValue({
+      ...formValue,
+      title: menuName,
+      price,
+      category: categoryValue,
+    });
+    onClose();
+  };
+
+  useEffect(() => {
+    console.log({ formValue });
+  }, [formValue]);
   return (
-    <Dialog open={open}>
-      <DialogTitle>DAILOG</DialogTitle>
-      <Button onClick={() => onClose('Messege Send to Parent Element')}>Close</Button>
+    <Dialog sx={{}} open={open}>
+      <DialogTitle sx={{ padding: '12px' }}>
+        {title}
+        <IconButton
+          sx={{
+            position: 'absolute',
+            right: 8,
+            top: 8,
+            color: (theme) => theme.palette.grey[500],
+          }}
+          onClick={() => onClose()}
+        >
+          <CloseIcon />
+        </IconButton>
+      </DialogTitle>
+      <Divider />
+      <Box
+        sx={{
+          padding: '1rem',
+          width: '500px',
+        }}
+      >
+        <Stack spacing={2}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <FormControl fullWidth>
+                <TextField value={menuName} size="small" fullWidth id="name" label="Name" variant="outlined" aria-label="name" onChange={(e) => setMenuName(e.target.value)} />
+              </FormControl>
+            </Grid>
+            <Grid item xs={6}>
+              <FormControl>
+                <InputLabel htmlFor="price">Price</InputLabel>
+                <OutlinedInput
+                  size="small"
+                  id="price"
+                  value={price}
+                  onChange={(e) => setPrice(e.target.value)}
+                  startAdornment={<InputAdornment position="start">Rp.</InputAdornment>}
+                  label="Price"
+                  type="number"
+                />
+              </FormControl>
+            </Grid>
+            <Grid item xs={6}>
+              <FormControl fullWidth>
+                <Autocomplete
+                  value={categoryValue.label}
+                  onChange={(e, newValue) => {
+                    setCategoryValue(newValue.label);
+                  }}
+                  inputValue={categoryInputValue}
+                  onInputChange={(e, newInputValue) => {
+                    setCategoryInputValue(newInputValue);
+                  }}
+                  size="small"
+                  disablePortal
+                  id="combo-box-demo"
+                  options={category}
+                  renderInput={(params) => <TextField {...params} label="Category" />}
+                />
+              </FormControl>
+            </Grid>
+          </Grid>
+          {/* <TextField size="small" fullWidth id="category" label="Category" variant="outlined" type={'number'} aria-label="category" /> */}
+        </Stack>
+      </Box>
+      <Divider />
+      <Box sx={{ padding: '.5rem 1rem' }}>
+        <Stack spacing={2} direction={'row'}>
+          <Button onClick={() => onClose()} sx={{ marginLeft: 'auto' }} variant="outlined" color="error">
+            Cancel
+          </Button>
+          <Button type="submit" onClick={handleSubmit} variant="contained">
+            Add Menu
+          </Button>
+        </Stack>
+      </Box>
     </Dialog>
   );
 };
+
+const category = [{ label: 'food' }, { label: 'drink' }, { label: 'snack' }];
 
 export default InputMenuDialog;
--- src/feature/menu/Menu.jsx
+++ src/feature/menu/Menu.jsx
@@ -9,31 +9,11 @@ import MenuCard from './MenuCard';
 import MenuCategoriesCard from './MenuCategoriesCard';
 import InputMenuDialog from './InputMenuDialog';
 
-const category = [
-  {
-    name: 'all',
-    icon: 'https://cdn-icons.flaticon.com/png/512/738/premium/738079.png?token=exp=1661163091~hmac=9f2cde47a9c2e6c083344818bb17f097',
-  },
-  {
-    name: 'food',
-    icon: 'https://cdn-icons-png.flaticon.com/512/857/857681.png',
-  },
-  {
-    name: 'drink',
-    icon: 'https://cdn-icons-png.flaticon.com/512/4329/4329538.png',
-  },
-];
-
-// !important [DON'T DELETE] = this function to get uniq singgel array from array of object
-// const menuCategories = [...new Set(data.map((item) => item.category))];
-
-// !important [DON'T DELETE] this function to get unic object from array of object depends the key of object
-// const newData = [...new Map(data.map((item) => [item['category'], item])).values()];
-
 const Menu = () => {
   const [openAddMenuDialog, setOpenAddMenuDialog] = useState(false);
   const dispatch = useDispatch();
   const { isLoading, data, error } = useSelector((state) => state.menu.menu);
+  const [dialogData, setDialogData] = useState({});
 
   useEffect(() => {
     dispatch(fetchAllMenu('all'));
@@ -45,10 +25,14 @@ const Menu = () => {
 
   const heandleAddMenuDialogOpen = () => {
     setOpenAddMenuDialog(true);
+    setDialogData({
+      title: 'Add New Menu',
+      icon: <AddIcon />,
+    });
   };
   const heandleAddMenuDialogClose = (fromDialog) => {
-    console.log({ fromDialog });
     setOpenAddMenuDialog(false);
+    setDialogData({});
   };
 
   return (
@@ -78,7 +62,7 @@ const Menu = () => {
           >
             Add New Menu
           </Button>
-          <InputMenuDialog open={openAddMenuDialog} onClose={heandleAddMenuDialogClose} />
+          <InputMenuDialog open={openAddMenuDialog} onClose={heandleAddMenuDialogClose} data={dialogData} />
         </Stack>
         <Divider />
         <Stack direction={'row'} spacing={2}>
@@ -88,5 +72,26 @@ const Menu = () => {
     </>
   );
 };
+
+const category = [
+  {
+    name: 'all',
+    icon: 'https://cdn-icons.flaticon.com/png/512/738/premium/738079.png?token=exp=1661163091~hmac=9f2cde47a9c2e6c083344818bb17f097',
+  },
+  {
+    name: 'food',
+    icon: 'https://cdn-icons-png.flaticon.com/512/857/857681.png',
+  },
+  {
+    name: 'drink',
+    icon: 'https://cdn-icons-png.flaticon.com/512/4329/4329538.png',
+  },
+];
+
+// !important [DON'T DELETE] = this function to get uniq singgel array from array of object
+// const menuCategories = [...new Set(data.map((item) => item.category))];
+
+// !important [DON'T DELETE] this function to get unic object from array of object depends the key of object
+// const newData = [...new Map(data.map((item) => [item['category'], item])).values()];
 
 export default Menu;

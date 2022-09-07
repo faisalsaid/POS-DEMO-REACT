270283e0cb16da7c8518e909c49e79e7a16b6304
faisalsaid <falsdesain@gmail.com>
2022-09-07 14:45:23 +0900

    add createAsyncThunk to order slice to handle fetch all menu

--- src/feature/order/sliceOrder.js
+++ src/feature/order/sliceOrder.js
@@ -1,13 +1,38 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+export const fetchAllMenu = createAsyncThunk('order/fetchAllMenu', (data) => {
+  const filter = data === 'all' ? '' : `?category=${data}`;
+  return axios.get(`${process.env.REACT_APP_API_SOURCE}menu${filter}`).then((resp) => resp.data);
+});
 
 const initialState = {
-  orderMenuList: [],
+  menu: {
+    isLoading: false,
+    data: [],
+    error: '',
+  },
 };
 
 const orderSlice = createSlice({
   name: 'order',
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchAllMenu.pending, (state, action) => {
+      state.menu.isLoading = true;
+    });
+    builder.addCase(fetchAllMenu.fulfilled, (state, { payload }) => {
+      state.menu.isLoading = false;
+      state.menu.data = payload;
+      state.menu.error = '';
+    });
+    builder.addCase(fetchAllMenu.rejected, (state, action) => {
+      state.menu.isLoading = false;
+      state.menu.data = [];
+      state.menu.error = action.error.message;
+    });
+  },
 });
 
 export default orderSlice.reducer;

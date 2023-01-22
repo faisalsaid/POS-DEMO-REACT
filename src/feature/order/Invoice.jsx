e07459f45ddbf513b0aaeea2dc9111330933d74a
faisalsaid <falsdesain@gmail.com>
2023-01-22 15:22:48 +0900

    create invoiceCardlist ui

--- src/feature/order/Invoice.jsx
+++ src/feature/order/Invoice.jsx
@@ -3,27 +3,31 @@ import { useSelector, useDispatch } from 'react-redux';
 import Main from '../../components/layout/Main';
 import { fetchAllOrder } from './sliceOrder';
 import { Stack, Typography, Box, Divider } from '@mui/material';
+import InvoiceCardList from './InvoiceCardList';
 
 const Invoice = () => {
   const { isLoading, data: allOrders, error } = useSelector((state) => state.order.orders);
   const dispatch = useDispatch();
-  console.log(allOrders);
+  // console.log(allOrders);
 
   useEffect(() => {
     dispatch(fetchAllOrder());
   }, []);
   return (
     <Main title="Invoice">
-      <Stack direction={'row'} spacing={2}>
+      <Stack
+        sx={{
+          // width: '100%',
+          flexWrap: 'wrap',
+          gap: '1rem',
+        }}
+        direction={'row'}
+        spacing={2}
+      >
         {allOrders
           .filter((order) => order.isPaidOff === false)
           .map((order, index) => {
-            return (
-              <Box key={index}>
-                <Typography>{order.customer}</Typography>
-                <Typography>{order.totalAmount}</Typography>
-              </Box>
-            );
+            return <InvoiceCardList order={order} key={index} />;
           })}
       </Stack>
     </Main>
--- src/feature/order/InvoiceCardList.jsx
+++ src/feature/order/InvoiceCardList.jsx
@@ -1,7 +1,100 @@
 import React from 'react';
+import { Stack, Typography, Button, Divider } from '@mui/material';
+import InfoIcon from '@mui/icons-material/Info';
+import currencyFormatter from 'currency-formatter';
+import { Box } from '@mui/system';
 
-const InvoiceCardList = () => {
-  return <div>InvoiceCardList</div>;
+const InvoiceCardList = (props) => {
+  //   console.log(props);
+  const { order } = props;
+  console.log(order);
+
+  return (
+    <Stack
+      spacing={1}
+      sx={{
+        bgcolor: 'whitesmoke',
+        borderRadius: '4px',
+      }}
+    >
+      <Stack
+        sx={{
+          padding: '1rem',
+        }}
+      >
+        <Stack direction={'row'} justifyContent="space-between" spacing={1}>
+          <Box
+            sx={{
+              border: `1px solid ${order.isPaidOff ? 'green' : 'red'}`,
+              padding: '.2rem 1rem',
+              borderRadius: '4px',
+              color: `${order.isPaidOff ? 'green' : 'red'}`,
+            }}
+          >
+            Not Pay
+          </Box>
+          <Button
+            sx={{
+              width: '100px',
+            }}
+            size="small"
+            onClick={() => alert(`${order.orderRef}`)}
+            startIcon={<InfoIcon />}
+            color="success"
+            variant="outlined"
+            fullWidth
+          >
+            Details
+          </Button>
+        </Stack>
+        <Stack
+          sx={{
+            paddingTop: '1rem',
+            paddingBottom: '1rem',
+          }}
+        >
+          <Typography color="success" variant="caption" component={'p'}>
+            Ref :
+          </Typography>
+          <Typography color={'success'} variant="body2" component={'p'}>
+            {order.orderRef}
+          </Typography>
+        </Stack>
+        <Divider />
+        <Stack
+          sx={{
+            paddingTop: '1rem',
+            paddingBottom: '1rem',
+          }}
+        >
+          <Typography variant="caption" component={'p'}>
+            Customer :
+          </Typography>
+          <Typography variant="body" component={'h3'}>
+            {order.customer}
+          </Typography>
+        </Stack>
+        <Divider />
+        <Stack
+          sx={{
+            paddingTop: '1rem',
+            paddingBottom: '1rem',
+          }}
+        >
+          <Typography variant="caption" component={'p'}>
+            Total :
+          </Typography>
+          <Typography variant="h5" component={'p'}>
+            {currencyFormatter.format(order.totalAmount, { code: 'IDR' })}
+            {/* {order.totalAmount} */}
+          </Typography>
+        </Stack>
+        <Button variant="contained" color="success">
+          Proccess
+        </Button>
+      </Stack>
+    </Stack>
+  );
 };
 
 export default InvoiceCardList;

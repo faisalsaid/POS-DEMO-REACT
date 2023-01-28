85508a042c27cb5ad8807a1e77f27cd12e199037
faisalsaid <falsdesain@gmail.com>
2023-01-29 01:46:48 +0900

    update card dashboar ui

--- src/components/utility/DashboardCard.jsx
+++ src/components/utility/DashboardCard.jsx
@@ -1,6 +1,33 @@
+import { Grid, Stack, Typography } from '@mui/material';
+import { Box } from '@mui/system';
+import currencyFormatter from 'currency-formatter';
+
 const DashboardCard = (props) => {
   const { data } = props;
-  return <div>DashboardCard</div>;
+  return (
+    <Stack
+      sx={{ padding: '1rem', minWidth: '300px', backgroundImage: data.background ? data.background : 'whitesmoke', borderRadius: '4px', boxShadow: '1px 3px 3px rgba(0,0,0,0.1)' }}
+    >
+      <Grid container spacing={2} sx={{ marginBottom: '1rem' }}>
+        <Grid item xs={8}>
+          <Stack>
+            <Typography color={'white'} variant="overline" component={'h6'}>
+              {data.title}
+            </Typography>
+            <Typography color={'white'} variant="h5" component={'p'}>
+              {data.value.isCurency ? currencyFormatter.format(data.value.value, { code: 'IDR' }) : data.value.value}
+            </Typography>
+          </Stack>
+        </Grid>
+        <Grid color={'white'} item xs={4}>
+          {data.icon}
+        </Grid>
+      </Grid>
+      <Typography color={'white'} variant="caption">
+        {data.description}
+      </Typography>
+    </Stack>
+  );
 };
 
 export default DashboardCard;
--- src/feature/dashboard/Dashboard.jsx
+++ src/feature/dashboard/Dashboard.jsx
@@ -1,7 +1,45 @@
 import Main from '../../components/layout/Main';
 import { Typography, Stack, Divider } from '@mui/material';
+import DashboardCard from '../../components/utility/DashboardCard';
+
+// ICONS
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
+import RedeemIcon from '@mui/icons-material/Redeem';
 
 const Dashboard = () => {
+  const card1 = {
+    background: 'linear-gradient(-90deg,rgba(205,233,144,1) 0%, rgba(170,203,115,1) 100%)',
+    title: 'Today',
+    value: {
+      isCurency: true,
+      value: 40000,
+    },
+    description: 'Halo Stangger is New Era',
+    icon: <AttachMoneyIcon sx={{ fontSize: '3rem' }} />,
+  };
+
+  const card2 = {
+    background: 'linear-gradient(-90deg, rgba(250,211,231,1) 0%, rgba(233,142,173,1) 100%)',
+    title: 'Sale Items',
+    value: {
+      isCurency: false,
+      value: 80,
+    },
+    description: 'Halo Stangger is New Era',
+    icon: <RedeemIcon sx={{ fontSize: '3rem' }} />,
+  };
+
+  const card3 = {
+    background: 'linear-gradient(-90deg, rgba(251,194,82,1) 0%, rgba(255,177,0,1) 100%)',
+    title: 'Total Earnings',
+    value: {
+      isCurency: true,
+      value: 4345540,
+    },
+    description: 'Halo Stangger is New Era',
+    icon: <AttachMoneyIcon sx={{ fontSize: '3rem' }} />,
+  };
+
   return (
     <Main title="Dashboard">
       <Stack>
@@ -10,7 +48,11 @@ const Dashboard = () => {
         </Typography>
       </Stack>
       <Divider />
-      <Stack direction={'row'} gap={1}></Stack>
+      <Stack direction={'row'} gap={1} sx={{ padding: '1rem' }}>
+        <DashboardCard data={card1} />
+        <DashboardCard data={card2} />
+        <DashboardCard data={card3} />
+      </Stack>
     </Main>
   );
 };
